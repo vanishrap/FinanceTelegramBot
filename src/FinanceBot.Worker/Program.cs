80f455c2d6e0e604e6cbf9bd8101872551d4c73f
vanishrap <vanishrap@gmail.com>
2026-08-30 22:56:29 +0800

    Fix Telegram API request URLs

--- src/FinanceBot.Worker/Program.cs
+++ src/FinanceBot.Worker/Program.cs
@@ -4,6 +4,7 @@ using FinanceBot.Infrastructure.OpenAI;
 using FinanceBot.Infrastructure.Persistence;
 using FinanceBot.Infrastructure.Services;
 using FinanceBot.Infrastructure.Telegram;
+using FinanceBot.Worker.Configuration;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 
